d1542671a148be1cbecebd0fdbb85875bee493dd
dtovar <jtovar.ks2.tech>
2024-02-18 21:58:14 -0400

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,55 @@
+import styled from '@emotion/styled'
+import useSelectMonedas from '../hooks/useSelectMonedas';
+import { monedas } from '../data/monedas';
+import { useEffect } from 'react'
+
+const InputSubmit = styled.input`
+  background-color: #9497FF;
+  border: none;
+  width: 100%;
+  padding: 10px;
+  color: #fff;
+  margin-top: 30px;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 20px;
+  border-radius: 5px;
+  transition: background-color .3s ease;
+
+  &:hover {
+    background-color: #7A7DFE;
+    cursor: pointer;
+  }
+`;
+
+const Formulario = () => {
+
+    const [ moneda, SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas);
+
+    useEffect(() =>{
+        const consultarAPI = async () =>{
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
+
+            console.log(resultado.Data);
+        }
+        consultarAPI();
+    }, [])
+
+  return (
+    <form>
+
+        <SelectMonedas/>
+
+
+    
+        <InputSubmit 
+         type="submit" 
+         value="cotizar"
+        />
+    </form>
+  )
+}
+
+export default Formulario
